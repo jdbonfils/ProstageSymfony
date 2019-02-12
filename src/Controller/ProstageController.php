ad4f80199d72435668e9a00516b0b185b0bb3faf
Jean deBonfils <jdbonfils@iutbayonne.univ-pau.fr>
2019-02-12 10:27:24 +0100

    processus de soumission du formulaire

--- src/Controller/ProstageController.php
+++ src/Controller/ProstageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\constraints\NotBlank;
 use App\Entity\Entreprise;
 use App\Entity\Formation;
 use App\Entity\Stage;
@@ -80,18 +81,31 @@ class ProstageController extends AbstractController
 	/**
      * @Route("/ajouterEntreprise", name="ajouterEntreprise")
      */
-    public function ajouterEntreprise()
+    public function ajouterEntrepriseRequest( /*$request, ObjectManager $manager*/)
     {
         //Création d'une entreprise
         $entreprise = new Entreprise() ;
         //Création d'un objet formulaire
         $formulaireEntreprise = $this -> createFormBuilder($entreprise)
-                                      -> add('nom')
+                                      -> add('nom',TextType::class , ['constraints' => new NotBlank()])
                                       -> add('activite')
-                                      -> add('adresse')
+                                      -> add('adresse', ['constraints' => new NotBlank()])
                                       -> getForm();
 
         $vueFormulaire = $formulaireEntreprise->createView() ;
+
+
+      /*  $formulaireEntreprise->handleRequest($request);
+         if ($formulaireEntreprise->isSubmitted() )
+         {
+            // Mémoriser la date d'ajout de la ressources
+            $entreprise->setDateAjout(new \dateTime());
+            // Enregistrer la ressource en base de donnéelse
+            $manager->persist($entreprise);
+            $manager->flush();
+            // Rediriger l'utilisateur vers la page d'accueil
+            return $this->redirectToRoute('openclassdut_accueil');
+         }*/
 
         return $this->render('prostage/ajouterEntreprise.html.twig',['vueFormulaire'=> $vueFormulaire]);
         
